3f484876249f78371891473b1a3b83ad753fe0e4
Oleksandr Horievy <alexandr.gorevoy07@gmail.com>
2018-02-10 12:31:35 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/catalog/');
+define('DIR_SYSTEM', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/');
+define('DIR_IMAGE', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/image/');
+define('DIR_LANGUAGE', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/catalog/language/');
+define('DIR_TEMPLATE', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/config/');
+define('DIR_CACHE', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/storage/download/');
+define('DIR_LOGS', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/PROGRAMS/OpenServer/OSPanel/domains/opencart.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
